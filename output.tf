77a74265364934970163da2610fd35c1c4a45cd0
Karim Ali <karim.alaraby@gmail.com>
2020-09-23 17:51:31 +0200

    adding SG for EKS cluster

--- output.tf
+++ output.tf
@@ -7,3 +7,11 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = local.cluster_name
 }
+
+output "security_group_cluster" {
+  value = "${aws_security_group.cluster.id}"
+}
+
+output "security_group_node" {
+  value = "${aws_security_group.node.id}"
+}
